0d88a6a39021389b25245fb751ab4c3488910ec4
RomaRomanenko <romanenko2312@gmail.com>
2013-12-19 20:21:40 +0200

    Table for add users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,38 +1,38 @@
 class UsersController < ApplicationController
-  
-	def index
-    	@users = User.all
-	end
-  
-	def new
-    	@user = User.new
-	end
 
-	def create
-    	@user = User.new(params[:user])
-      render :new unless @user.save
-end
+  def index
+    @users = User.all
+  end
 
-	def show
-    	@user = User.find(params[:id])
-	end
+  def new
+    @user = User.new
+  end
 
-	def edit
-    	@user = User.find(params[:id])
-	end
+  def create
+    @user = User.new(params[:user])
+    render :new unless @user.save
+  end
 
-  	def update
-    	@user = User.find(params[:id])
-    	if @user.update_attributes(params[:user])
-      		redirect_to user_path
-    	else
-      		render :edit
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(params[:user])
+      redirect_to user_path
+    else
+      render :edit
     end
-end
+  end
 
-  	def destroy
-    		User.find(params[:id]).destroy
-      	redirect_to users_path
-  	end
+  def destroy
+    User.find(params[:id]).destroy
+    redirect_to users_path
+  end
 
-end
+ end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,4 @@ class User < ActiveRecord::Base
 
   validates :first_name, :last_name, presence: true
   validates :email, presence: true, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i }
-
 end
