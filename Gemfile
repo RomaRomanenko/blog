0079ffe8a9fe411f8329ccc65ae826f3ba6edd01
RomaRomanenko <romanenko2312@gmail.com>
2013-12-24 12:20:21 +0200

    Del to show

--- Gemfile
+++ Gemfile
@@ -8,7 +8,7 @@ gem 'rails', '3.2.13'
 gem 'mysql2'
 gem 'pry'
 gem 'twitter-bootstrap-rails'
-
+gem 'bootstrap-sass', '2.1'
 
 # Gems used only for assets and not required
 # in production environments by default.
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,21 +13,13 @@ class UsersController < ApplicationController
     render :new unless @user.save
   end
 
-  def show
-    @user = User.find(params[:id])
-  end
-
   def edit
     @user = User.find(params[:id])
   end
 
   def update
     @user = User.find(params[:id])
-    if @user.update_attributes(params[:user])
-      redirect_to user_path
-    else
-      render :edit
-    end
+    render :edit unless @user.update_attributes(params[:user])
   end
 
   def destroy
